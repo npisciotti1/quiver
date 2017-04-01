e7643676e7b0a15521e30470772bad3aebf0d0c4
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-01 16:54:42 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(PORT, () => {
+  console.log('Quiver online!');
+});
