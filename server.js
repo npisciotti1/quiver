b53f41ba0248871e20b90c638827b34d719d3817
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-01 17:07:22 -0700

    messing with server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.static(`${__dirname}/build`));
+app.use(`${__dirname}/build`);
 
 app.listen(PORT, () => {
   console.log('Quiver online!');
