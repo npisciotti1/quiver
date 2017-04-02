34abf663c983ef916c5c6d2ab51a13ace7bbcdd4
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-01 17:15:42 -0700

    hotfix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+//TODO: configure this to work with deployment
 app.use(express.static(`${__dirname}/build`));
 
 app.listen(PORT, () => {
