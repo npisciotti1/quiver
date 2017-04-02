716ac33f832099c7a44c571f125f341ea6dfcb14
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-02 11:52:37 -0700

    added log-config.js and route-config.js

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
 
 //TODO: configure this to work with deployment
-// app.use(express.static(`${__dirname}/build`));
+app.use(express.static(`${__dirname}/build`));
 
-const path = require('path');
-
-module.exports = {
-  app: function () {
-    const app = express();
-    const indexPath = path.join(__dirname, '/../index.html');
-    const publicPath = express.static(path.join(__dirname, '../public'));
-
-    app.use('/public', publicPath);
-    app.get('/', function (_, res) {
-      res.sendFile(indexPath);
-    });
-
-    return app;
-  }
-};
+app.listen(PORT, () => {
+  console.log('Quiver initialized');
+});
