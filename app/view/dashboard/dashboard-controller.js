04cef18e577e5754791402095d2b8cc2fa3ecd11
Shiv <shiv.unadkat@gmail.com>
2017-04-13 18:03:47 -0700

    venue service giggity good to go and along with the create venue component

---
+++ app/view/dashboard/dashboard-controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('./_dashboard.scss');
+
+//TODO: Build out our home view as we add functionality
+module.exports = ['$log', DashboardController];
+
+function DashboardController($log) {
+  $log.debug('DashboardController');
+}
