ad748359fee70c3e7dbc048bd04aab2da6372547
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-18 16:57:03 -0700

    got our gear table to fetch on page reload, also got tabs working to display each table

--- app/component/gear/display-gear/display-gear.js
+++ app/component/gear/display-gear/display-gear.js
@@ -14,7 +14,47 @@ module.exports = {
 function DisplayGearController($log, $window, venueService, gearService) {
   $log.debug('DisplayGearController');
 
-  this.gear = gearService.userGear;
 
-  this.gearArray = Object.keys(this.gear);
+  this.populateTable = function() {
+    $log.debug('DisplayGearController.getGear()');
+
+    gearService.fetchGear($window.localStorage.currentVenue)
+    .then( gearData => {
+      this.gear = gearData;
+      this.gearArray = Object.values(this.gear);
+
+
+      this.audioArray = this.gearArray[0];
+      this.lightingArray = this.gearArray[1];
+      this.stageArray = this.gearArray[2];
+    });
+  };
+
+  this.showAudio = function() {
+    $log.debug('DisplayGearController.showAudio()');
+
+    this.displayAudio = true;
+    this.displayLighting = false;
+    this.displayStage = false;
+  };
+
+  this.showLighting = function() {
+    $log.debug('DisplayGearController.showLighting()');
+
+    this.displayAudio = false;
+    this.displayLighting = true;
+    this.displayStage = false;
+  };
+
+  this.showStage = function() {
+    $log.debug('DisplayGearController.showStage()');
+
+    this.displayAudio = false;
+    this.displayLighting = false;
+    this.displayStage = true;
+  };
+
+  //set this to true to display audio table on page load
+  this.displayAudio = true;
+  this.populateTable();
 }
