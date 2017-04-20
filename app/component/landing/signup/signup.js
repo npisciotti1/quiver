f21807ad969e2529720795061950fd8430624753
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-04-20 14:10:46 -0700

    added more styles and hard-coded gear-info

--- app/component/landing/signup/signup.js
+++ app/component/landing/signup/signup.js
@@ -22,24 +22,72 @@ function SignupController($log, $q, $window, $location, authService, venueServic
     gear: {
       audio: [
         {
-          name: 'example instrument',
-          description: 'so cool instrument'
+          name: 'FOH Console',
+          description: 'Venue SC48 Digital Protools console'
+        },
+        {
+          name: 'FOH Speakers',
+          description: '(10) JBL VTX V20 – LINE ARRAYED'
+        },
+        {
+          name: 'FOH Speakers',
+          description: '(4) JBL AC28 Fills'
+        },
+        {
+          name: '5-Piece Kit',
+          description: 'Pearl - Session Studio Classic'
+        },
+        {
+          name: 'Deluxe Strat',
+          description: 'Play like Jimi!'
+        },
+        {
+          name: 'Mics',
+          description: '(2) Shure Beta 91A'
+        },
+        {
+          name: 'Mics',
+          description: '(2) Shure Beta 52A'
         },
       ],
       lighting: [
         {
-          name: 'sweet light',
-          description: 'this is bright'
+          name: 'Controller Board',
+          description: 'Avolites Quartz'
+        },
+        {
+          name: 'LED Cans',
+          description: '(18) Chauvet Par-Quad 18 LEDs'
+        },
+        {
+          name: 'LED Cans',
+          description: '(6) Chauvet NXT-1 Moving Head LED Panel'
+        },
+        {
+          name: 'FX',
+          description: '(1) Martin-Rush MH3 Beam'
+        },
+        {
+          name: 'FX',
+          description: '(1) Martin Atomic Strobe'
         }
       ],
       stage: [
         {
           name: 'width',
-          description: 'example width'
+          description: '32 feet'
+        },
+        {
+          name: 'depth',
+          description: '16.5 feet'
         },
         {
           name: 'height',
-          description: 'example height'
+          description: '3.5 feet'
+        },
+        {
+          name: 'Clearance',
+          description: '9.5 feet to the lights'
         }
       ]
     }
